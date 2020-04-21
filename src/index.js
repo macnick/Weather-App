afee3076d4c863fa2eff7c288a7d34805cb23f34
macnick <nick@macexperts.gr>
2020-04-21 19:02:59 +0300

    Structure Javascript

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-console.log('Hello Weather');
-// alert('Everything OK');
-async function geo() {
-  await navigator.geolocation.getCurrentPosition(
-    (pos) => {
-      console.log(pos);
-    },
-    (err) => console.log('Here is my error: ', err.message)
-  );
-}
+const ui = (() => {
+  const renderData = (weather, unit = 'C') => {
+    const { temp, feels_like, temp_min, temp_max } = weather.main;
+    let name = weather.name;
+    const { description, icon } = weather.weather[0];
+    console.log(temp_min, temp_max, name, description, icon);
+    document.getElementById(
+      'location'
+    ).innerText = `${name}, ${weather.sys.country}`;
+    document.getElementById('description').innerText = `${description}`;
+    document.getElementById('temp').innerText = `${~~temp} º${unit}`;
+    document.getElementById('feels').innerText = `Feels like: ${feels_like}`;
+  };
 
-console.log(geo());
+  return { renderData };
+})();
+
+const data = (() => {
+  const getWeather = async (city = 'Athens', unit = 'metric') => {
+    let KEY = '4e3a46238240176b8e18b2672de5b78b';
+    const weather = await fetch(
+      ` https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&APPID=${KEY}`,
+      { mode: 'cors' }
+    );
+    const weatherData = await weather.json();
+    // console.log(weatherData);
+    return weatherData;
+  };
+
+  return {
+    getWeather,
+  };
+})();
+
+const control = ((ui, data) => {
+  const myWeather = async (city) => {
+    let response = await data.getWeather();
+    ui.renderData(response);
+    console.log('in control', response);
+    return response;
+  };
+  myWeather();
+})(ui, data);
